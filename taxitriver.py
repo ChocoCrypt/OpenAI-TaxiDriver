1258d2b8a8d646a26c6399162c73601c2c14a91a
chococrypt <chocokrypt@gmail.com>
2022-02-24 00:41:57 -0500

    matrix with 100% acuracy

--- taxitriver.py
+++ taxitriver.py
@@ -1,5 +1,6 @@
 import gym
 import time
+import datetime
 import pickle
 from tqdm import tqdm
 import numpy as np
@@ -8,21 +9,31 @@ import numpy as np
 
 alfa = 0.1
 gamma = 0.6
-epsilon = 0.01
+epsilon = 1.0
 
 env = gym.make("Taxi-v3")
 env.reset()
-epochs = 15_000
+epochs = 15000
 
 Q = np.zeros([env.observation_space.n , env.action_space.n])
+date = datetime.datetime.now()
+year = date.year
+month = date.month
+day = date.day
+hour = date.hour
 
 
 for i in tqdm(range(epochs)):
     done = False
     new_state = env.reset()
     while(not done):
-        action = env.action_space.sample()
-        #action = np.argmax(Q[new_state , :])
+        #action = env.action_space.sample()
+        #epsilon gradient policy
+        rand = np.random.uniform(0,1)
+        if(rand < epsilon):
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(Q[new_state])
         valor_viejo = Q[new_state , action]
         estado_viejo = new_state # esto hay que guardarlo
         new_state , reward,  done , info = env.step(action)
@@ -32,8 +43,6 @@ for i in tqdm(range(epochs)):
         estado_viejo = new_state
 
 
-with open(f"Q_matrix_trained_{epochs}_on_{time.time()}.pkl","wb") as file:
-    pickle.dump(Q,file)
 
 
 wins = 0
@@ -44,8 +53,11 @@ for i in tqdm(range(1000)):
     while(not done):
         action = np.argmax(Q[new_state])
         new_state, reward , done , info = env.step(action)
-        env.render()
+        #env.render()
         if(reward > -1):
             wins += 1
 
 print(wins)
+acc = wins/1000
+with open(f"Q_matrix_trained_on_{year}-{month}-{day}_at_{hour}_accuracy_{acc}.pkl","wb") as file:
+    pickle.dump(Q,file)
